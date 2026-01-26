8b98e665a2147aa1679e7a98bf122e50858f9fbe
Sergio Sánchez Ramírez <mofeing+github@gmail.com>
2026-01-26 17:58:32 +0100

    test integration with OMEinsum

--- test/runtests.jl
+++ test/runtests.jl
@@ -67,6 +67,7 @@ end
         @safetestset "Python" include("integration/python.jl")
         @safetestset "Optimisers" include("integration/optimisers.jl")
         @safetestset "FillArrays" include("integration/fillarrays.jl")
+        @safetestset "OMEinsum" include("integration/omeinsum.jl")
         if ENZYMEJAX_INSTALLED[] && !Sys.isapple()
             @safetestset "EnzymeJAX Export" include("integration/enzymejax.jl")
         end
